bbce969318f824395f7e4532b1fa77273c46243e
southpaw203 <73884858+vlaex@users.noreply.github.com>
2022-07-08 15:18:04 +0300

    Update build scripts

--- build-scripts/build-production.ts
+++ build-scripts/build-production.ts
@@ -6,6 +6,14 @@ import { version } from "../package.json";
 build({
   ...buildOptions,
   outdir: "dist",
-  minify: true
+  minify: true,
+  banner: {
+    js: `
+    /*
+    * Copyright (c) 2022 vlaex7@gmail.com
+    * See LICENSE.md for details
+    */
+    `
+  }
 })
   .then(_ => console.info(`production build ${version} succeed`));
