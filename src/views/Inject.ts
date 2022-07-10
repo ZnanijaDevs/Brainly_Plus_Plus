8dbfa3674a6615ae5d01bd7d1c7ca98ee51fb6f9
southpaw203 <73884858+vlaex@users.noreply.github.com>
2022-07-10 17:20:06 +0300

    Update Inject.ts

--- src/views/Inject.ts
+++ src/views/Inject.ts
@@ -6,18 +6,24 @@ import { getUserAuthToken } from "@utils/getViewer";
 
 import ReplaceModerationButtons from "./Task/Moderation";
 
-const addWindowLoadedListener = (handler: () => void) => {
-  window.onload = handler;
-};
+/**
+ * Inject files (content scripts and stylesheets) to DOM
+ * @param files An array of files
+ */
+async function InjectToDOM(files: string[]) {
+  const jsFiles = files.filter(file => file.match(/\.js$/));
+  const cssFiles = files.filter(file => file.match(/\.css$/));
+
+  if (cssFiles.length) await ToBackground("InjectStyles", cssFiles);
+  if (jsFiles.length) await ToBackground("InjectScripts", jsFiles);
+}
 
 class Core {
   private viewerAuthToken: string;
 
   private path = window.location.href;
-  private jsFiles: string[] = [];
-  private cssFiles: string[] = [];
 
-  private Path(pattern: RegExp): boolean {
+  private checkRoute(pattern: RegExp): boolean {
     return pattern.test(this.path);
   }
 
@@ -28,89 +34,40 @@ class Core {
   async Init() {
     await this.AuthUser();
 
-    if (this.Path(/\/$|(predmet\/\w+$)/)) {
-      this.addInjectedFiles([
+    if (this.checkRoute(/\/$|(predmet\/\w+$)/)) {
+      await InjectToDOM([
         "content-scripts/HomePage/index.js",
         "styles/HomePage/index.css"
       ]);
     }
 
-    if (this.Path(/\/task\/\d+/)) {
+    if (this.checkRoute(/\/task\/\d+/)) {
       ReplaceModerationButtons();
 
-      this.addInjectedFiles([
+      await InjectToDOM([
         "content-scripts/Task/index.js",
         "styles/Task/index.css"
       ]);
     }
 
-    this.addInjectedFiles([
-      // "content-scripts/Shared/ModerationPanel/index.js",
+    await InjectToDOM([
       "styles/ModerationTicket/index.css"
     ]);
-
-    addWindowLoadedListener(this.injectFiles.bind(this));
-
-  
-    /*if (this.Path(/\/users\/user_content\/[1-9]\d*\/?/)) {
-      this.AddInjectedFiles([
-        "content-scripts/UserContent/index.js"
-      ]);
-    }
-
-    if (this.Path(/\/task\/\d+/)) {
-      this.AddInjectedFiles([
-        "content-scripts/Task/index.js",
-        "styles/Task/index.css"
-      ]);
-    }
-
-    if (this.Path(/\/profil\/[A-Za-zа-яА-Я0-9]+-\d+/)) {
-      this.AddInjectedFiles([
-        "content-scripts/UserProfile/index.js"
-      ]);
-    }
-
-    this.AddInjectedFiles([
-      "content-scripts/Shared/ModerationPanel/index.js",
-      "styles/ModerationPanel/index.css",
-      "styles/ModerationTicket/index.css"
-    ]);*/
   }
 
   async AuthUser() {
     const userToken = await getUserAuthToken();
     if (!userToken) {
-      addWindowLoadedListener(() => 
-        Flash("error", locales.youDoNotHavePermissionToUseThisExt, {
-          sticky: true,
-          withIcon: true
-        })
-      );
+      Flash("error", locales.youDoNotHavePermissionToUseThisExt, {
+        sticky: true,
+        withIcon: true
+      });
 
       throw Error("Seems like you are not authorized");
     }
 
     this.viewerAuthToken = userToken;
-  }
-
-  addInjectedFiles(files: string[]) {
-    this.jsFiles.push(
-      ...files.filter(file => file.match(/\.js$/))
-    );
-    this.cssFiles.push(
-      ...files.filter(file => file.match(/\.css$/))
-    );
-  }
-
-  injectFiles() {
-    if (this.cssFiles.length) ToBackground("InjectStyles", this.cssFiles);
-    ToBackground("InjectScripts", this.jsFiles);
-
-    console.log(
-      chalk.blue.bgWhite("[Brainly++] Injected files"),
-      { css: this.cssFiles, js: this.jsFiles }
-    );
+    console.log(chalk.bgRed.bold("User token"), userToken);
   }
 }
 
